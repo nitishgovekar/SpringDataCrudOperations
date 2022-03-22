bc5e68b3b8b41c5955171e1bfd25e6a20a63faa8
nitish.govekar <nitishgovekar1992@gmail.com>
2022-03-22 09:07:26 +0530

    Created new project to implement Associations Mappings

---
+++ associations/src/main/java/com/springdata/associations/onetomany/Customer.java
@@ -0,0 +1,47 @@
+package com.springdata.associations.onetomany;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String name;
+	
+	@OneToMany(mappedBy = "customer")
+	private Set<PhoneNumber> phoneNumbers;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<PhoneNumber> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(Set<PhoneNumber> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+
+}
